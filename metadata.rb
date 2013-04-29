a4e203892d74c2fb8a0336d7f9c54af37e2a5c2d
Guilhem Lettron <guilhem@lettron.fr>
2013-04-29 12:54:18 +0200

    change recommends to depends fix FC007

--- metadata.rb
+++ metadata.rb
@@ -7,9 +7,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version          "0.0.2"
 
-recommends       "ark"
-recommends       "windows"
-recommends       "chocolatey"
+depends          "ark"
+depends          "windows"
+depends          "chocolatey"
 
 supports         "ubuntu"
 supports         "windows"
