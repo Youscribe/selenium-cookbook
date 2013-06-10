e248d92af39c6a472c6b7a90b64db32a16dd4531
Guilhem Lettron <guilhem@lettron.fr>
2013-06-10 12:18:15 +0200

    ChromeDriver 2.0

--- attributes/chromedriver.rb
+++ attributes/chromedriver.rb
@@ -5,11 +5,11 @@ when "linux"
   default['selenium']['chromedriver']['directory'] = "/usr/local/bin"
   case node["kernel"]["machine"]
   when "x86_64"
-    default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver2_linux64_0.8.zip"
+    default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver_linux64_2.0.zip"
   else
-    default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver_linux32_26.0.1383.0.zip"
+    default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver_linux32_2.0.zip"
   end
 when "windows"
   default['selenium']['chromedriver']['directory'] = 'C:\selenium'
-  default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver2_win32_0.8.zip"
+  default["selenium"]["chromedriver"]["url"] = "https://chromedriver.googlecode.com/files/chromedriver_win32_2.0.zip"
 end
