03d1103edbaaab86762fef2ab1d560ce14e52ac4
Guilhem Lettron <guilhem@lettron.fr>
2013-06-10 12:23:44 +0200

    release v0.0.3

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "Apache 2.0"
 description      "Install selenium"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          "0.0.2"
+version          "0.0.3"
 
 depends          "ark"
 depends          "windows"
